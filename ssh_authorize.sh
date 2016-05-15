4242ff849f3c31862581587a559a04f68fd93f4d
ycqiu <429148848@qq.com>
2016-05-15 16:53:55 +0800

    Update ssh_authorize.sh

--- ssh_authorize.sh
+++ ssh_authorize.sh
@@ -7,7 +7,7 @@ then
 fi
 
 umask 077
-test -d ~/.ssh || mkdir ~/.ssh;
+test -d ~/.ssh || mkdir ~/.ssh
 test -f ~/.ssh/id_rsa.pub || ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub | ssh $@ "umask 077; test -d ~/.ssh || mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys"
 
